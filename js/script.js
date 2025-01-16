5f4e381dd5420228cde5febcffefbb44beab95e3
Lorenzo-Ruben-Rampa <lorenzo.rampa.ruben@gmail.com>
2025-01-16 17:32:30 +0100

    creazione snack 2

--- js/script.js
+++ js/script.js
@@ -6,3 +6,41 @@
 // Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
 // Snack 3 (Bonus)
 // Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b). La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
+
+// Snack 1
+const bici = [
+    { nome : 'ciclocross',
+    peso : 7},
+    { nome : 'ebike',
+    peso : 25
+    },
+    { nome: 'endurance',
+    peso : 8
+    }
+    ];
+
+for (let propBici in bici) {
+    const messaggio = `La bici col peso minore è ${bici[propBici]} e pesa ${propBici}`;
+    console.log(messaggio);
+}
+
+    // console.log(bici[0].peso, bici[1].peso, bici[2].peso);
+    // console.log(Math.min(...bici));
+
+// Snack 2
+const squadre = [
+    { nome : 'Monza',
+    punti : 0,
+    falli_subiti : 0},
+    { nome : 'Juve',
+    punti : 0,
+    falli_subiti : 0},
+    { nome: 'Milan',
+    punti : 0,
+    falli_subiti : 0},   
+    { nome: 'Inter',
+    punti : 0,
+    falli_subiti : 0},   
+];
+
+// Snack 3
