1e074b296a5a45a76b759b578b21f31ec5256527
Lorenzo-Ruben-Rampa <lorenzo.rampa.ruben@gmail.com>
2025-01-17 07:02:55 +0100

    bonus completato

--- js/script.js
+++ js/script.js
@@ -82,7 +82,22 @@ console.log(nomiFalli);
 
 // // Snack 3
 // Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b). La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
-// a < bici
-// []
 
-// return
+const array = ["case", "libri", "auto", "viaggi", "foglidigiornale"];
+const a = 2;
+const b = 5;
+
+//Scrivo la funzione
+function estraiPosizione(array, a, b) {
+        return array.slice(a, b + 1)
+}
+
+const risultatoPosizione = estraiPosizione(array, a, b)
+
+// Creo un nuovo array
+const newArray = [];
+
+// Metto il risultato della funzione nell'array
+newArray.push(risultatoPosizione);
+
+console.log(newArray);
