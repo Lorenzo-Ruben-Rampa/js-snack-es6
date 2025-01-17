592076e6adb7bc1251aed6e35b5e424aa9644bc4
Lorenzo-Ruben-Rampa <lorenzo.rampa.ruben@gmail.com>
2025-01-17 10:34:41 +0100

    correzione in classe

--- js/script.js
+++ js/script.js
@@ -3,38 +3,35 @@
 // Snack2
 
 const bici = [
-    { nome : 'ciclocross',
-    peso : 7},
-    { nome : 'ebike',
-    peso : 25
+    { 
+    nome : 'ciclocross',
+    peso : 9
     },
-    { nome: 'endurance',
+    {
+    nome : 'ebike',
+    peso : 7
+    },
+    {
+    nome: 'endurance',
     peso : 8
     }
     ];
 
-// Creo il nuovo array
-const pesi = [];
+// // Riempio il nuovo array
+// for (let i = 0; i < bici.length; i++) {
+//     pesi.push({ nome: bici[i].nome, peso: bici[i].peso }); // pesi.push(bici[i])  -> se faccio nel secondo modo, se cambia un oggetto nel primo array, cambia anche nel secondo
+// }
 
-// Riempio il nuovo array
-for (let i = 0; i < bici.length; i++) {
-    pesi.push({ nome: bici[i].nome, peso: bici[i].peso }); // pesi.push(bici[i])
-}
-
-// Prendo la bici più òeggera
-let biciLeggera = pesi[0];
-    for (let i = 0; i < pesi.length; i++) {
-        if (pesi[i].peso < biciLeggera.peso) {
-            biciLeggera = pesi[i];
+// Prendo la bici più leggera
+let biciLeggera = bici[0];
+    for (let i = 0; i < bici.length; i++) {
+        if (bici[i].peso < biciLeggera.peso) {
+            biciLeggera = bici[i];
         }
     }
 
-    console.log('La bici con il peso minore è:', biciLeggera);
-
     const messaggio = `La bici col peso minore è ${biciLeggera.nome} e pesa ${biciLeggera.peso}`;
     console.log(messaggio);
-
-// let pesoMinimo = Math.min(...bici);
 
 // // Snack 2
 // Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti. Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
@@ -42,18 +39,26 @@ let biciLeggera = pesi[0];
 // Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
 
 const squadre = [
-    { nome : 'Monza',
-    punti : 0,
-    falli_subiti : 0},
-    { nome : 'Juve',
-    punti : 0,
-    falli_subiti : 0},
-    { nome: 'Milan',
-    punti : 0,
-    falli_subiti : 0},   
-    { nome: 'Inter',
-    punti : 0,
-    falli_subiti : 0},   
+    { 
+        nome : 'Monza',
+        punti : 0,
+        falli_subiti : 0
+    },
+    { 
+        nome : 'Juve',
+        punti : 0,
+        falli_subiti : 0
+    },
+    {
+        nome: 'Milan',
+        punti : 0,
+        falli_subiti : 0
+    },   
+    { 
+        nome: 'Inter',
+        punti : 0,
+        falli_subiti : 0
+    },   
 ];
 
 //Creo una funzione per generare un numero casuale
@@ -61,11 +66,12 @@ const generaNumeroCasuale = (min, max) => Math.floor(Math.random() * (max - min 
 
 //Ciclo l'array e imposto condizione se valore = a 0
 for (let i = 0; i < squadre.length; i++) {
-for (let prop in squadre[i]) {
-    if (squadre[i][prop] === 0) {
-        squadre[i][prop] = generaNumeroCasuale(1, 100);
+
+    for (let prop in squadre[i]) {
+        if (squadre[i][prop] === 0) {
+            squadre[i][prop] = generaNumeroCasuale(1, 100);
+        }
     }
-}
 }
 
 // Creo un nuovo array
@@ -74,8 +80,8 @@ const nomiFalli = [];
 //Ciclo l'array squadre 
 for (let i = 0; i < squadre.length; i++) {
 
-//Metto nell'array i nomi e i falli subiti
-nomiFalli.push({ nome: squadre[i].nome, falli_subiti: squadre[i].falli_subiti });
+    //Metto nell'array i nomi e i falli subiti
+    nomiFalli.push({ nome: squadre[i].nome, falli_subiti: squadre[i].falli_subiti });
 }
 
 console.log(nomiFalli);
@@ -89,15 +95,9 @@ const b = 5;
 
 //Scrivo la funzione
 function estraiPosizione(array, a, b) {
-        return array.slice(a, b + 1)
+        return array.slice(a, b)
 }
 
 const risultatoPosizione = estraiPosizione(array, a, b)
 
-// Creo un nuovo array
-const newArray = [];
-
-// Metto il risultato della funzione nell'array
-newArray.push(risultatoPosizione);
-
-console.log(newArray);
+console.log(risultatoPosizione);
