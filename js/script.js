17c43bb0c5883692d6bfd35ca6a32dca3b4a66b4
Lorenzo-Ruben-Rampa <lorenzo.rampa.ruben@gmail.com>
2025-01-17 06:15:18 +0100

    Es 1 completato

--- js/script.js
+++ js/script.js
@@ -1,13 +1,7 @@
 // Snack 1
 // Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. Stampare a schermo la bici con peso minore.
 // Snack2
-// Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti. Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
-// Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
-// Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
-// Snack 3 (Bonus)
-// Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b). La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
 
-// Snack 1
 const bici = [
     { nome : 'ciclocross',
     peso : 7},
@@ -19,15 +13,34 @@ const bici = [
     }
     ];
 
-for (let propBici in bici) {
-    const messaggio = `La bici col peso minore è ${bici[propBici]} e pesa ${propBici}`;
-    console.log(messaggio);
+// Creo il nuovo Array
+const pesi = [];
+
+// Riempio il nuovo Array
+for (let i = 0; i < bici.length; i++) {
+    pesi.push({ nome: bici[i].nome, peso: bici[i].peso }); // pesi.push(bici[i])
 }
 
-    // console.log(bici[0].peso, bici[1].peso, bici[2].peso);
-    // console.log(Math.min(...bici));
+// Prendo la bici più òeggera
+let biciLeggera = pesi[0];
+    for (let i = 0; i < pesi.length; i++) {
+        if (pesi[i].peso < biciLeggera.peso) {
+            biciLeggera = pesi[i];
+        }
+    }
 
-// Snack 2
+    console.log('La bici con il peso minore è:', biciLeggera);
+
+    const messaggio = `La bici col peso minore è ${biciLeggera.nome} e pesa ${biciLeggera.peso}`;
+    console.log(messaggio);
+
+// let pesoMinimo = Math.min(...bici);
+
+// // Snack 2
+// Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti. Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+// Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
+// Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
 const squadre = [
     { nome : 'Monza',
     punti : 0,
@@ -43,4 +56,10 @@ const squadre = [
     falli_subiti : 0},   
 ];
 
-// Snack 3
+// Creo un nuovo array
+const nomiFalli = [];
+
+
+
+// // Snack 3
+// Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b). La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
